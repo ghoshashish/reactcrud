6de2253d21e46708622cd3a6bd085df0f7366f1c
ghoshashish <ghoshashish43@gmail.com>
2021-07-17 21:52:30 +0530

    added components expenses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: "299.9",
+      date: new Date(2021, 21, 6),
+    },
+    {
+      title: "Bike Insurance",
+      amount: "199.9",
+      date: new Date(2022, 23, 8),
+    },
+  ];
   return (
     <div className="App">
-      <h1> Ashish Ghosh</h1>
+      <h1> Expense Items</h1>
+      <Expenses items={expenses} />
     </div>
   );
 }

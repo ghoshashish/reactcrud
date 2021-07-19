0bdce7fcf124802b0028070a0798c26559162363
ghoshashish <ghoshashish43@gmail.com>
2021-07-19 22:35:24 +0530

    added index and App css files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import "./App.css";
 
 const DUMMY_EXPENSES = [
   {
